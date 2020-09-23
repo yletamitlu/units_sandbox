09e969537a9653829e76d2a58e9d4cbc374a0b06
Viktoriia Kamoldinova <v.kamoldinova@corp.mail.ru>
2020-09-23 22:05:37 +0300

    add code

--- src/isMono.js
+++ src/isMono.js
@@ -1,5 +1,19 @@
 /** Проверка массива на монотонность */
 /** Например, isMono([0, 1, 5, 9, 15]) вернет true */
 export function isMono(values) {
+  let increase = true;
+  let decrease = true;
+  let i = 0;
 
+  for (i; i < values.length - 1; ++i) {
+    if (values[i] > values[i+1]) {
+      increase = false;
+    }
+
+    if (values[i] < values[i+1]) {
+      decrease = false;
+    }
+  }
+
+  return increase || decrease;
 }
