53503051128d731e52d41e47ee7130b8c84c835e
Viktoriia Kamoldinova <v.kamoldinova@corp.mail.ru>
2020-09-23 21:55:23 +0300

    add tests

--- src/isMono.test.js
+++ src/isMono.test.js
@@ -9,6 +9,40 @@ describe('test isMono function', () => {
     assert.strictEqual(result, true);
   });
 
-  // Добавьте сюда ваши тесты
+  test('[1, 2, 3, 1]', () => {
+    const result = isMono([1, 2, 3, 1]);
+
+    assert.strictEqual(result, false);
+  });
+
+  test('[1, 1, 1]', () => {
+    const result = isMono([1, 1, 1, 2, 3]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test('[1, 1, 1, 2, 3, 1]', () => {
+    const result = isMono([1, 1, 1, 2, 3, 1]);
+
+    assert.strictEqual(result, false);
+  });
+
+  test('[1, 1, 2, 2, 3]', () => {
+    const result = isMono([1, 1, 2, 2, 3]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test('[3, 2, 1]', () => {
+    const result = isMono([3, 2, 1]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test('[3, 2, 1, 2]', () => {
+    const result = isMono([3, 2, 1, 2]);
+
+    assert.strictEqual(result, false);
+  });
 })
 
