840c9383b700706c04e439492ce1f51e95bb0979
yletamitlu <catherinaleksandrovna@gmail.com>
2021-03-11 03:28:37 +0400

    Написать isMono

--- src/isMono.js
+++ src/isMono.js
@@ -1,5 +1,15 @@
 /** Проверка массива на монотонность */
 /** Например, isMono([0, 1, 5, 9, 15]) вернет true */
-export function isMono(values) {
 
+export function isMono(values) {
+    const asc = (values[0] <= values[values.length - 1]);
+    let result = true;
+    values.forEach((item, index) => {
+        if (values[index + 1]) {
+            const is = asc ? item <= values[index + 1] : item >= values[index + 1];
+            if (!is) result = false;
+        }
+    });
+
+    return result;
 }
